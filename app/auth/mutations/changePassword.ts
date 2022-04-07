38fedc831a6dbfe2d937eb9a143b6cbb285b375f
Ryan Jung <ryan.matthew.jung@gmail.com>
2022-04-07 16:08:56 -0400

    fix: inject db in ChangePassword

--- app/auth/mutations/changePassword.ts
+++ app/auth/mutations/changePassword.ts
@@ -1,19 +1,20 @@
 import { NotFoundError, SecurePassword, resolver } from "blitz"
-import db from "db"
 import { authenticateUser } from "./login"
 import { ChangePassword } from "../validations"
+import { injectDb } from "app/core/custom-resolvers/resolvers"
 
 export default resolver.pipe(
+  injectDb,
   resolver.zod(ChangePassword),
   resolver.authorize(),
   async ({ currentPassword, newPassword }, ctx) => {
-    const user = await db.user.findFirst({ where: { id: ctx.session.userId! } })
+    const user = await ctx.db.user.findFirst({ where: { id: ctx.session.userId! } })
     if (!user) throw new NotFoundError()
 
-    await authenticateUser(user.email, currentPassword)
+    await authenticateUser(user.email, currentPassword, ctx)
 
     const hashedPassword = await SecurePassword.hash(newPassword.trim())
-    await db.user.update({
+    await ctx.db.user.update({
       where: { id: user.id },
       data: { hashedPassword },
     })
