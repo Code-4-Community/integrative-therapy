e6ebfe30733a4eef3bf3ee7b79f7f32c482d5ab6
Ryan Jung <ryan.matthew.jung@gmail.com>
2022-04-16 17:31:57 -0400

    feat: test:e2e

---
+++ test/factories/user.ts
@@ -0,0 +1,22 @@
+import signup from "app/auth/mutations/signup"
+import Chance from "chance"
+import { PromiseReturnType } from "next/types/utils"
+
+const chance = new Chance()
+
+const ctx = {
+  session: { $create: () => {} },
+}
+
+type UserAttributes = {
+  email?: string
+  password: string
+}
+
+export const user = async ({ email = chance.email(), password }: UserAttributes) => {
+  const user = await signup({ email, password }, ctx as any)
+
+  return user
+}
+
+export type User = PromiseReturnType<typeof user>
