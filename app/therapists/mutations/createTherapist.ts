42a509adb4dd025bf88887e53280890a592a3052
Ryan Jung <ryan.matthew.jung@gmail.com>
2022-04-23 16:50:18 -0400

    fix: remove unused

--- app/therapists/mutations/createTherapist.ts
+++ app/therapists/mutations/createTherapist.ts
@@ -1,12 +1,12 @@
 import { resolver } from "blitz"
-import db from "db"
+// import db from "db"
 import { z } from "zod"
 
 const CreateTherapist = z.object({
   name: z.string(),
 })
 
-export default resolver.pipe(resolver.zod(CreateTherapist), resolver.authorize(), async (input) => {
+export default resolver.pipe(resolver.zod(CreateTherapist), resolver.authorize(), async (_) => {
   // TODO: in multi-tenant app, you must add validation to ensure correct tenant
   // const therapist = await db.therapist.create({ data: input })
   // return therapist
